93901bd0a1c7b6196080ff4bb826a23e4dd3d3e1
rodrigoroseo <rodrigo.roseo@gmail.com>
2021-08-04 12:16:31 -0300

    Lista 3 exercícios java

--- java/AULAS/src/Lista3Exercicios/Lista3Exercicio1.java
+++ java/AULAS/src/Lista3Exercicios/Lista3Exercicio1.java
@@ -5,7 +5,7 @@ public class Lista3Exercicio1 {
 		
 		for(int numero=1000;numero<2000;numero++) {
 			if(numero % 11 == 5){
-				System.out.printf("%d | %d \n",numero);
+				System.out.printf("%d\n",numero);
 			}
 		}
 		
