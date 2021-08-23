4eb82a2ccb27df1d08df0e2e75d52abb5c73f01b
rodrigoroseo <rodrigo.roseo@gmail.com>
2021-08-23 13:10:16 -0300

    Primeiro programa em Spring

---
+++ java/hello/src/main/java/com/helloworld/hello/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.helloworld.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "<h3>Mentalidade:</h3> <li>Persistência</li><h3>Habilidade:</h3> <li>Atenção aos detalhes</li>";
+	}
+	@GetMapping("/bye")
+	public String sayBye() {
+		return "<h3>Objetivos de aprendizagem:</h3><ul><li>Aprender Spring</li><li>Treinar minha gestão de tempo</li></ul>";
+	}
+}
